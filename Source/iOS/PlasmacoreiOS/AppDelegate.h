dfb4fa4b7fd18a56c9990dbc84a87ae932667b1a
Abe Pralle <Abe.Pralle@plasmaworks.com>
2015-04-13 22:11:00 -0700

    Created iOS project.

---
+++ Source/iOS/PlasmacoreiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlasmacoreiOS
+//
+//  Created by Abe Pralle on 4/13/15.
+//  Copyright (c) 2015 Abe Pralle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Source/iOS/PlasmacoreiOS/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  PlasmacoreiOS
+//
+//  Created by Abe Pralle on 4/13/15.
+//  Copyright (c) 2015 Abe Pralle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
