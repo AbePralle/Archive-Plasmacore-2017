564570498c13a5e178826a5cb244066649c39888
Abe Pralle <Abe.Pralle@gmail.com>
2016-03-13 14:04:13 -0700

    Created Plasmacore Studio IDE project in Swift.

---
+++ Source/IDE/Mac/PlasmacoreStudio/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PlasmacoreStudio
+//
+//  Created by Abraham Pralle on 3/13/16.
+//  Copyright © 2016 Abe Pralle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
