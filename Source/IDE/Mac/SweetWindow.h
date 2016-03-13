acd61f1442b893f15127123dd5c6ae661c7557e5
Abe Pralle <Abe.Pralle@gmail.com>
2016-03-13 13:57:58 -0700

    Added .gitattributes, SweetWindow test.

---
+++ Source/IDE/Mac/SweetWindow.h
@@ -0,0 +1,13 @@
+//
+//  SweetWindow.h
+//  PlasmacoreStudio
+//
+//  Created by Abraham Pralle on 3/12/16.
+//  Copyright © 2016 Abe Pralle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SweetWindow : NSWindowController
+
+@end
