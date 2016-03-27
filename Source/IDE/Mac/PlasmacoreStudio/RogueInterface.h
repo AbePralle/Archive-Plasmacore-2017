36afb96ddb61980490ba1458a82e159e03e44581
Abe Pralle <Abe.Pralle@gmail.com>
2016-03-26 20:35:57 -0700

    Got Windows opening again.

--- Source/IDE/Mac/PlasmacoreStudio/RogueInterface.h
+++ Source/IDE/Mac/PlasmacoreStudio/RogueInterface.h
@@ -7,11 +7,13 @@
 extern "C" {
 #endif
 
-void         RogueInterface_configure();
-void         RogueInterface_launch();
-NSData*      RogueInterface_send_messages( const unsigned char* data, int count );
-void         RogueInterface_set_arg_count( int count );
-void         RogueInterface_set_arg_value( int index, const char* value );
+NSWindowController* Plasmacore_create_window( NSString* name );
+
+void    RogueInterface_configure();
+void    RogueInterface_launch();
+NSData* RogueInterface_send_messages( const unsigned char* data, int count );
+void    RogueInterface_set_arg_count( int count );
+void    RogueInterface_set_arg_value( int index, const char* value );
 
 #ifdef __cplusplus
 } // extern "C"
