6104efc37adf0f555ec057c742212c0d6cd4f7ac
Abe Pralle <Abe.Pralle@gmail.com>
2016-05-12 12:26:06 -0700

    Added PNG, JPEG, FreeType, and HarfBuzz libraries to iOS project.

---
+++ Source/Platforms/iOS/LibHarfBuzz/LibHarfBuzz.h
@@ -0,0 +1,13 @@
+//
+//  LibHarfBuzz.h
+//  LibHarfBuzz
+//
+//  Created by Abraham Pralle on 5/12/16.
+//  Copyright © 2016 Abe Pralle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibHarfBuzz : NSObject
+
+@end
---
+++ Source/Platforms/iOS/LibJPEG/LibJPEG.h
@@ -0,0 +1,13 @@
+//
+//  LibJPEG.h
+//  LibJPEG
+//
+//  Created by Abraham Pralle on 5/12/16.
+//  Copyright © 2016 Abe Pralle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibJPEG : NSObject
+
+@end
