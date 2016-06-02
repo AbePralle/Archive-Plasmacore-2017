eb71b42da68f026d8512b54457efa051ab143ef6
Abe Pralle <Abe.Pralle@gmail.com>
2016-06-01 23:18:17 -0700

    Updated Rogue Plasmacore library for Rogue changes.

--- Source/Plasmacore/Swift/RogueInterface.mm
+++ Source/Plasmacore/Swift/RogueInterface.mm
@@ -1,8 +1,8 @@
 #include "RogueInterface.h"
 #include "RogueProgram.h"
-#include "Starbright.h"
-#include "StarbrightGL.h"
-#include "SuperCPPResourceBank.h"
+//#include "Starbright.h"
+//#include "StarbrightGL.h"
+//#include "SuperCPPResourceBank.h"
 
 #include <cstdio>
 #include <cstring>
@@ -11,7 +11,7 @@ using namespace std;
 static int          RogueInterface_argc = 0;
 static const char** RogueInterface_argv = {0};
 
-SuperCPP::ResourceBank<Starbright::Renderer*> starbright_renderers;
+//SuperCPP::ResourceBank<Starbright::Renderer*> starbright_renderers;
 
 NSString* Plasmacore_rogue_string_to_ns_string( RogueString* st )
 {
@@ -30,7 +30,7 @@ RogueString* Plasmacore_ns_string_to_rogue_string( NSString* st )
 extern "C" RogueString* Plasmacore_find_asset( RogueString* filepath )
 {
   NSString* ns_name = Plasmacore_rogue_string_to_ns_string( filepath );
-  NSString* ns_filepath = [[NSBundle mainBundle] pathForResource:ns_name ofType:nil]; 
+  NSString* ns_filepath = [[NSBundle mainBundle] pathForResource:ns_name ofType:nil];
   if (ns_filepath == nil) return 0;
   return Plasmacore_ns_string_to_rogue_string( ns_filepath );
 }
@@ -59,7 +59,7 @@ extern "C" NSData* RogueInterface_send_messages( const unsigned char* data, int 
   // Call Rogue MessageManager.update(), which sends back a reference to another byte
   // list containing messages to us.
   list = RoguePlasmacore__MessageManager__update( mm );
-  
+
   if ( !list ) return [NSData data];
   return [[NSData alloc] initWithBytes:list->data->bytes length:list->count];
 }
@@ -81,25 +81,30 @@ extern "C" void RogueInterface_set_arg_value( int index, const char* value )
 
 int Starbright_create_renderer()
 {
-  return starbright_renderers.add( new Starbright::GLRenderer() );
+return 0;
+  //return starbright_renderers.add( new Starbright::GLRenderer() );
 }
 
 void Starbright_begin_draw( int renderer_id, int display_width, int display_height )
 {
+/*
   Starbright::Renderer* renderer = starbright_renderers.get( renderer_id );
   if (renderer)
   {
     renderer->activate();
     renderer->begin_draw( display_width, display_height );
   }
+*/
 }
 
 void Starbright_end_draw( int renderer_id )
 {
+/*
   Starbright::Renderer* renderer = starbright_renderers.get( renderer_id );
   if (renderer)
   {
     renderer->end_draw();
   }
+*/
 }
 
