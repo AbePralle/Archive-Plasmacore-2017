505f0fef00716abf6ba908e0931af7ae34ff6db3
Abe Pralle <Abe.Pralle@gmail.com>
2016-09-15 22:39:37 -0700

    Shifted iOS image loading into PlasmacoreImageIO framework.

---
+++ Libraries/Frameworks/PlasmacoreImageIO/PlasmacoreImageIO/PlasmacoreImageIO.h
@@ -0,0 +1,13 @@
+//
+//  PlasmacoreImageIO.h
+//  PlasmacoreImageIO
+//
+//  Created by Abraham Pralle on 9/14/16.
+//  Copyright © 2016 Plasmaworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlasmacoreImageIO : NSObject
+
+@end
