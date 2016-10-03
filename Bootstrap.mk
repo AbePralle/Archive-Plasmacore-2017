23d07c1565fa01cae2822b1e26bf6168a84bf4e5
Abe Pralle <Abe.Pralle@gmail.com>
2016-10-03 11:16:03 -0700

    README update

--- Bootstrap.mk
+++ Bootstrap.mk
@@ -1,8 +1,9 @@
-bootstrap:
+bootstrap_plasmacore:
 	@[ -e Source ]    && echo "ERROR: Source/ folder exists; aborting bootstrap to prevent possible overwrite." && exit 1 || true
 	@[ -e Platforms ] && echo "ERROR: Platforms/ folder exists; aborting bootstrap to prevent possible overwrite." && exit 1 || true
 	@[ ! -e Bootstrap/Plasmacore ] && echo "Cloning Plasmacore master branch into Bootstrap/Plasmacore/" && mkdir -p Bootstrap && cd Bootstrap && git clone git@github.com:AbePralle/Plasmacore.git || true
 	rm -rf Bootstrap/Plasmacore/.git
 	@rsync -a --out-format="Copying %n%L" Bootstrap/Plasmacore/* .
+	@rsync -a --out-format="Copying %n%L" Bootstrap/Plasmacore/.??* .
 	rm -rf Bootstrap
 	@echo "Done!"
