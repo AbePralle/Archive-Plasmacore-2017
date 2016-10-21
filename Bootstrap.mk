85b4e3147b2ba979730e50e49f8b8bd577204e7f
Abe Pralle <Abe.Pralle@gmail.com>
2016-10-20 22:38:40 -0700

    Bootstrap.mk now excludes 'README.md' from being copied.

--- Bootstrap.mk
+++ Bootstrap.mk
@@ -3,7 +3,7 @@ bootstrap_plasmacore:
 	@[ -e Platforms ] && echo "ERROR: Platforms/ folder exists; aborting bootstrap to prevent possible overwrite." && exit 1 || true
 	@[ ! -e Bootstrap/Plasmacore ] && echo "Cloning Plasmacore master branch into Bootstrap/Plasmacore/" && mkdir -p Bootstrap && cd Bootstrap && git clone git@github.com:AbePralle/Plasmacore.git || true
 	rm -rf Bootstrap/Plasmacore/.git
-	@rsync -a --out-format="Copying %n%L" Bootstrap/Plasmacore/* .
+	@rsync -a --exclude="README.md" --out-format="Copying %n%L" Bootstrap/Plasmacore/* .
 	@rsync -a --out-format="Copying %n%L" --ignore-existing Bootstrap/Plasmacore/.gitattributes .
 	@rsync -a --out-format="Copying %n%L" --ignore-existing Bootstrap/Plasmacore/.gitignore .
 	rm -rf Bootstrap
