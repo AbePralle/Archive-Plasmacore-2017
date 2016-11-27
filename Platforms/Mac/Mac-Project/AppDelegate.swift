9e2510691fb404bcc16326d8d7f2c92369197d05
Abe Pralle <Abe.Pralle@gmail.com>
2016-11-27 10:39:50 -0800

    Started Mac project, removed old Libraries/Plasmacore/CPP.

---
+++ Platforms/Mac/Mac-Project/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mac-Project
+//
+//  Created by Abraham Pralle on 11/27/16.
+//  Copyright © 2016 Plasmaworks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
