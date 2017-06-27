70c61f128a51b0996e1c60661ff9e6f75ad7f61c
Abe Pralle <Abe.Pralle@gmail.com>
2017-06-27 08:19:10 -0700

    Added starter Android project.

---
+++ Platforms/Android/app/src/main/java/plasmacore/org/project/MainActivity.java
@@ -0,0 +1,30 @@
+package plasmacore.org.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+
+  // Used to load the 'native-lib' library on application startup.
+  static
+  {
+    System.loadLibrary("native-lib");
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+
+    // Example of a call to a native method
+    System.out.println(stringFromJNI());
+  }
+
+  /**
+   * A native method that is implemented by the 'native-lib' native library,
+   * which is packaged with this application.
+   */
+  public native String stringFromJNI();
+}
